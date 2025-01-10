a0afaa94d1fd30ae9134cbd25ee59da6d847dfb3
DevKolIn <donghyeon.study@gmail.com>
2025-01-10 16:19:14 +0900

    [🎉add] Init 함수 생성 및 난수용 시드 초기화

--- Manager/GameManager.cpp
+++ Manager/GameManager.cpp
@@ -2,27 +2,46 @@
 #include "../Monster/Monster.h"
 #include "../Monster/BossMonster.h"
 #include "../Player/Character.h"
+#include "../Monster/Troll.h"
+#include "../Monster/Orc.h"
+#include "../Monster/Goblin.h"
+#include "../Monster/BossMonster.h"
+#include <time.h>
+
+GameManager::GameManager()
+{
+    Init();
+}
+
+void GameManager::Init()
+{   
+	// 난수 초기화
+	srand(time(NULL));
+}
 
 GameManager::Monster* GameManager::GenerateMonster(int Level)
 {
 
-    return nullptr;
 }
 
 GameManager::BossMonster* GameManager::GenerateBossMonster(int Level)
-{
-
-    return nullptr;
+{ 
+    
 }
 
 void GameManager::Battle(Character* Player)
 {
+
 }
 
 void GameManager::VisitShop(Character* Player)
 {
+    
 }
 
 void GameManager::DisplayInventory(Character* Player)
 {
+
 }
+
+
--- Manager/GameManager.h
+++ Manager/GameManager.h
@@ -1,19 +1,22 @@
 ﻿#pragma once
 
+class Monster;
+class BossMonster;
+class Character;
 
 class GameManager
 {
 public:
     GameManager();
     ~GameManager();    
-    
-    class Monster;
-    class BossMonster;
-    class Character;
+
 public:
     Monster* GenerateMonster(int Level);
     BossMonster* GenerateBossMonster(int Level);
     void Battle(Character* Player);
     void VisitShop(Character* Player);
     void DisplayInventory(Character* Player);
+
+private:
+    void Init();
 };
