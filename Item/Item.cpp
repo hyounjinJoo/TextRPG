d2e1e74f77b34bcbf1a3b32b825fa2c1cb12fbd5
gwo1004 <mynameiskgw@gmail.com>
2025-01-10 15:08:05 +0900

    refactor : Item 클래스 인터페이스화

--- Item/Item.cpp
+++ Item/Item.cpp
@@ -1,12 +1,1 @@
 ﻿#include "Item.h"
-#include "../Player/Character.h"
-#include <iostream>
-
-Item::Item()
-{
-	std::cout << "Test Commit" << std::endl;
-}
-
-Item::~Item()
-{
-}
--- Item/Item.h
+++ Item/Item.h
@@ -1,13 +1,13 @@
 ﻿#pragma once
 #include <string>
+#include "../Player/Character.h"
 
 class Character;
 
 class Item
 {
 public:
-    Item();
-    virtual ~Item();
+    virtual ~Item() = default;
 
     virtual std::string GetName() = 0;
     virtual void Use(Character* Player) = 0;
